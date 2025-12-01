aef592d0141f56c8aa4196e9fadb897d729fe443
Matthew Wang <wangmd@umich.edu>
2025-12-01 18:59:16 -0500

    please speed I need this

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from joint_identifier import identify_joints
-from jutsu_matcher import run_jutsu_matcher
+from scale import get_normalized_hand_landmarks
+import numpy as np
 
+A = get_normalized_hand_landmarks("test_images/dumb_hand_sign.jpg")
+B = get_normalized_hand_landmarks("test_images/IMG_5377.jpg")
+D = get_normalized_hand_landmarks("test_images/hudson_ino_jutsu.jpg")
+E = get_normalized_hand_landmarks("test_images/alex_ino_jutsu.jpg")
+F = get_normalized_hand_landmarks("test_images/woman_hands.jpg")
 
-# identify_joints("test_images/dumb_hand_sign.jpg")
-run_jutsu_matcher("test_images/dumb_hand_sign_large.jpg")
+print(np.linalg.norm(A - B))
+print(np.linalg.norm(A - D))
+print(np.linalg.norm(A - E))
+print(np.linalg.norm(A - F))
